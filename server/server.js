49229a18ef82693ff196684d9e21fdd956c4d3b9
talbdbd <bendavid.bd.tal@gmail.com>
2024-07-17 17:54:12 -0700

    added node env to server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const nodeEnv = process.env.NODE_ENV || 'development';
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
